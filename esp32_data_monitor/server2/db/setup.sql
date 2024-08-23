3563db0aed54810bb33da9fbac6fb974763b86a3
manojccode <manojmanoj44445@gmail.com>
2024-08-23 20:36:25 +0530

    Delete esp32_data_monitor directory

--- esp32_data_monitor/server2/db/setup.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE esp_data;
-USE esp_data;
-CREATE TABLE sensor_data(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    temperature FLOAT,
-    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
